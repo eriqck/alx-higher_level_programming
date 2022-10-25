a00771a97a784bc71dd315c837248c69d1e8b24d
eriqck <ericdavid348@gmail.com>
2022-10-25 08:54:10 +0000

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
-
-rts.esrever = function (list) {
+#!/usr/bin/node
+exports.esrever = function (list) {
   return list.reduceRight(function (array, current) {
     array.push(current);
     return array;
