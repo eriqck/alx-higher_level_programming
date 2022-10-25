690b73164fe122daab6e96dd0c696078036170bd
eriqck <ericdavid348@gmail.com>
2022-10-25 08:44:31 +0000

    all

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+t dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
